a342df39fd20c60d0bd13488eb4b426b87e0b1cd
Giura Paul <giurapaul74@gmail.com>
2023-11-04 14:17:18 +0200

    Implemented CRUD operations for product handling

---
+++ Source/ProductInventoryManager.Backend/Migrations/20231103095523_NullablePropertiesEnabledOnProduct.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductInventoryManager.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullablePropertiesEnabledOnProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SupplierInformation",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductStockKeepingUnit",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductQuantity",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ProductPrice",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductName",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ProductImage",
+                table: "Products",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductDescription",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductCategory",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreationDate",
+                table: "Products",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SupplierInformation",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductStockKeepingUnit",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductQuantity",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ProductPrice",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductName",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ProductImage",
+                table: "Products",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductDescription",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductCategory",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreationDate",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
--- Source/ProductInventoryManager.Backend/Program.cs
+++ Source/ProductInventoryManager.Backend/Program.cs
@@ -19,4 +19,97 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/products", async (ProductInventoryManagerContext context) =>
+{
+    return await context.Products.ToListAsync();
+});
+
+app.MapGet("/products/{id}", async (ProductInventoryManagerContext context, int id) =>
+{
+    return await context.Products.FindAsync(id) is Product product ?
+        Results.Ok(product) :
+        Results.NotFound("This product doesn't exist.");
+});
+
+app.MapPost("/products/addProduct", async (ProductInventoryManagerContext context, Product product) =>
+{
+    context.Products.Add(product);
+    await context.SaveChangesAsync();
+    return Results.Ok(await context.Products.ToListAsync());
+});
+
+app.MapPut("/products/updateProduct/{id}", async (ProductInventoryManagerContext context, Product updatedProduct, int id) =>
+{
+    var product = await context.Products.FindAsync(id);
+    if (product == null)
+        return Results.NotFound($"The product with ID {id} could not be found.");
+
+    product.ProductName = updatedProduct.ProductName;
+    product.ProductDescription = updatedProduct.ProductDescription;
+    product.ProductCategory = updatedProduct.ProductCategory;
+    product.ProductStockKeepingUnit = updatedProduct.ProductStockKeepingUnit;
+    product.ProductPrice = updatedProduct.ProductPrice;
+    product.ProductQuantity = updatedProduct.ProductQuantity;
+    product.SupplierInformation = updatedProduct.SupplierInformation;
+    product.ProductImage = updatedProduct.ProductImage;
+    product.CreationDate = updatedProduct.CreationDate;
+
+    await context.SaveChangesAsync();
+
+    return Results.Ok(await context.Products.ToListAsync());
+});
+
+app.MapDelete("/products/{id}", async (ProductInventoryManagerContext context, int id) =>
+{
+    var product = await context.Products.FindAsync(id);
+    if (product == null)
+        return Results.NotFound($"The product with ID {id} could not be found.");
+
+    context.Products.Remove(product);
+    await context.SaveChangesAsync();
+
+    return Results.Ok(await context.Products.ToListAsync());
+});
+
+app.MapPost("/products/sellStock/{id}", async (ProductInventoryManagerContext context, int id, int quantityToSell) => 
+{
+    var product = await context.Products.FindAsync(id);
+
+    if (product == null)
+    {
+        return Results.NotFound($"The product with ID {id} could not be found.");
+    }
+
+    // Check if there is enough quantity in stock to sell
+    if (product.ProductQuantity < quantityToSell)
+    {
+        return Results.BadRequest("Not enough quantity in stock to fulfill the sale.");
+    }
+
+    // Deduct the sold quantity from the product's stock
+    product.ProductQuantity -= quantityToSell;
+
+    // Save the updated product information to the database
+    await context.SaveChangesAsync();
+
+    return Results.Ok($"Sale successful. Current {product.ProductName} stock: {product.ProductQuantity} items.");
+});
+
+app.MapPost("/products/addStock/{id}", async (ProductInventoryManagerContext context, int id, int quantityToAdd) =>
+{
+    var product = await context.Products.FindAsync(id);
+
+    if (product == null)
+        return Results.NotFound($"The product with ID {id} could not be found.");
+
+    if (quantityToAdd <= 0)
+        return Results.BadRequest("Sorry, you must provide a positive quantity number.");
+
+    product.ProductQuantity += quantityToAdd;
+
+    await context.SaveChangesAsync();
+
+    return Results.Ok($"Process successful. {quantityToAdd} item/s added to {product.ProductName} stock.");
+});
+
 app.Run();
