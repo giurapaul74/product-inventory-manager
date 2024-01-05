8fd8ac466d2329399c5bf8fb7a41ef132d9368e7
Giura Paul <giurapaul74@gmail.com>
2024-01-05 18:40:33 +0200

    Implemented Testing Project (W.I.P)

---
+++ Source/PIM.Tests/BackendServiceMock.cs
@@ -0,0 +1,17 @@
+﻿using Moq;
+using ProductInventoryManager.Backend;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIM.Tests
+{
+    public class BackendServiceMock : Mock<IBackendService>
+    {
+        public BackendServiceMock() : base(MockBehavior.Strict) 
+        {
+        }
+    }
+}
