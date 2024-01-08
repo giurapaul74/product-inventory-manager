6528d758648cce69bcbf8d26137add38ab9e5429
Giura Paul <giurapaul74@gmail.com>
2024-01-08 17:01:30 +0200

    Removed FrontendTests class (issues appeared, need to fix)

---
+++ Source/PIM.Tests/MockHttpMessageHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIM.Tests
+{
+    public class MockHttpMessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent("[]"),
+            });
+        }
+    }
+}
